f85afbcaf0582744cfc72891acb753a02a3c8903
Andrei Shikov <andreiishikov@gmail.com>
2017-04-16 23:39:57 +0300

    Add training procedure with split training

--- read_mat.py
+++ read_mat.py
@@ -2,10 +2,10 @@ import h5py
 import numpy as np
 import cv2
 
-data = h5py.File("samples/nyu_depth_v2_labeled.mat")
-
-depths = np.asarray(data.get("depths"))
-images = np.asarray(data.get("images"))
+first_set = h5py.File("samples/nyu_depth_data_labeled.mat")
+second_set = h5py.File("samples/nyu_depth_v2_labeled.mat")
+depths = np.concatenate((np.asarray(first_set.get("depths")), np.asarray(second_set.get("depths"))))
+images = np.concatenate((np.asarray(first_set.get("images")), np.asarray(second_set.get("images"))))
 
 print(depths.shape)
 print(images.shape)
@@ -20,6 +20,6 @@ for x in range(0, images.shape[0]):
             for k in range(0, image[i, j].shape[0]):
                 images_reshaped[x, k, j, i] = image[i, j, k]
 
-images_reshaped.tofile(open("samples/images.np", "wb"))
-depths.tofile(open("samples/depths.np", "wb"))
-np.array(1449).tofile(open("samples/length.np", "wb"))
+images_reshaped.tofile(open("samples/images-0.np", "wb"))
+depths.tofile(open("samples/depths-0.np", "wb"))
+np.array([len(images_reshaped)]).tofile(open("samples/length-0.np", "wb"))
